40700c6793d9e5e86356e5d3aae5b6072529f747
cqdetdev <101936396+cqdetdev@users.noreply.github.com>
2025-02-26 22:36:20 -0500

    chore: freeze config

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ config :prism, ecto_repos: [Data.Repo]
 config :prism, Data.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "Wallah123",
-  database: "oomf",
+  password: "tester",
+  database: "tester",
   hostname: "127.0.0.1",
   pool_size: 10
