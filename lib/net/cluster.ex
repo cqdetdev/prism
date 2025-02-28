ff467cd3f8a3d374fca4037dd9921811ba62641e
cqdetdev <101936396+cqdetdev@users.noreply.github.com>
2025-02-28 12:14:04 -0500

    feat: make it so server starts after certain amount of peer node retries

--- lib/net/cluster.ex
+++ lib/net/cluster.ex
@@ -1,6 +1,7 @@
 defmodule Net.Cluster do
   require Logger
   @retry_interval 500
+  @max_retries 5
 
   def connect(%{address: _address, peers: peers}) when is_list(peers) do
     Enum.each(peers, &attempt_connection/1)
@@ -11,16 +12,20 @@ defmodule Net.Cluster do
     :error
   end
 
-  defp attempt_connection(node) do
+  defp attempt_connection(node, retries \\ 0) do
     case Node.connect(node) do
       true ->
         Logger.info("Connected to distributed node: #{node}")
         :ok
 
       false ->
-        Logger.error("Failed to connect to #{node}. Retrying in #{@retry_interval} ms...")
-        Process.sleep(@retry_interval)
-        attempt_connection(node)
+        if retries < @max_retries do
+          Logger.error("Failed to connect to #{node}. Retrying in #{@retry_interval} ms...")
+          Process.sleep(@retry_interval)
+          attempt_connection(node, retries + 1)
+        else
+          Logger.error("Max retries exceeded for peer node #{node}")
+        end
     end
   end
 
